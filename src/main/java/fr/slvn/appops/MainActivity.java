2d3a2b7bcb5d7bc9f56d95b5222e439c9b505d15
slvn <sylvain.galand@gmail.com>
2013-11-05 23:58:39 +0100

    Show a Toast a prompt for uninstall when Exception raised

--- src/main/java/fr/slvn/appops/MainActivity.java
+++ src/main/java/fr/slvn/appops/MainActivity.java
@@ -1,16 +1,33 @@
 package fr.slvn.appops;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent("android.settings.APP_OPS_SETTINGS");
-        startActivity(intent);
+        launchAppOps();
         finish();
+    }
+
+    private void launchAppOps() {
+        try {
+            Intent intent = new Intent("android.settings.APP_OPS_SETTINGS");
+            startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(this, R.string.error_msg, Toast.LENGTH_LONG).show();
+            uninstallAppOps();
+        }
+    }
+
+    private void uninstallAppOps() {
+        Uri uri = Uri.parse("package:" + getPackageName());
+        Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, uri);
+        startActivity(intent);
     }
 }
