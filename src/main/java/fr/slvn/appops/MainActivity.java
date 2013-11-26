1da6b127990213fa4afa84c9b52e6925cee7744c
slvn <sylvain.galand@gmail.com>
2013-11-26 21:03:11 +0100

    Refactor AppOps launching using both methods

--- src/main/java/fr/slvn/appops/MainActivity.java
+++ src/main/java/fr/slvn/appops/MainActivity.java
@@ -16,22 +16,45 @@ public class MainActivity extends Activity {
     }
 
     private void launchAppOps() {
-        try {
-            Intent intent = new Intent();
-            intent.setClassName("com.android.settings", "com.android.settings.Settings");
-            intent.setAction(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_DEFAULT);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-            intent.putExtra(":android:show_fragment", "com.android.settings.applications.AppOpsSummary");
-
-            startActivity(intent);
-        } catch (Exception e) {
-            Toast.makeText(this, R.string.error_msg, Toast.LENGTH_LONG).show();
+        if (launchAppOpsViaAction()) {
+            // Classic method for 4.3 worked !
+        } else if (launchAppOpsViaComponentExtra()) {
+            // Tricky method from 4.4 worked !
+        } else {
+            // Cannot launch AppOps :(
             uninstallAppOps();
         }
     }
 
+    private boolean launchAppOpsViaAction() {
+        Intent intent = new Intent("android.settings.APP_OPS_SETTINGS");
+        return launchAppOps(intent);
+    }
+
+    private boolean launchAppOpsViaComponentExtra() {
+        Intent intent = new Intent();
+        intent.setClassName("com.android.settings", "com.android.settings.Settings");
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                        Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        intent.putExtra(":android:show_fragment", "com.android.settings.applications.AppOpsSummary");
+        return launchAppOps(intent);
+    }
+
+    protected boolean launchAppOps(Intent intent) {
+        try {
+            startActivity(intent);
+        } catch (Exception e) {
+            // Cannot launch activity !
+            return false;
+        }
+        return true;
+    }
+
     private void uninstallAppOps() {
+        Toast.makeText(this, R.string.error_msg, Toast.LENGTH_LONG).show();
         Uri uri = Uri.parse("package:" + getPackageName());
         Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, uri);
         startActivity(intent);
